c4b243250dff3fb683cca69bf0424c99f85aad24
alxawatcher <2593677381@qq.com>
2020-08-25 21:37:18 +0800

    rm

--- ADS1282文件/bf504f_Burn-master/!clean_19MHz.cmd
+++
@@ -1,3 +0,0 @@
-@rem Очистка каталога с загрузочными файлами
-@echo off
-..\Utils\gmake-378.exe  quartz_freq=19MHz clean
--- ADS1282文件/bf504f_Burn-master/!clean_8MHz.cmd
+++
@@ -1,3 +0,0 @@
-@rem Очистка каталога с загрузочными файлами
-@echo off
-..\Utils\gmake-378.exe clean QUARTZ_FREQ=8192000 
